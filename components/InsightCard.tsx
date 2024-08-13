e610a971dd335c813416a22183854a6e3b10408d
Somanshu Bhardwaj <89822689+somanshubhardwaj@users.noreply.github.com>
2024-08-13 19:22:00 +0530

    added feedback

--- components/InsightCard.tsx
+++ components/InsightCard.tsx
@@ -1,7 +1,17 @@
 "use client";
 import { motion } from "framer-motion";
 import { fadeIn } from "@/utils/motion";
-const InsightCard = ({ imgUrl, title, subtitle, index }) => (
+const InsightCard = ({
+  imgUrl,
+  title,
+  subtitle,
+  index,
+}: {
+  imgUrl: string;
+  title: string;
+  subtitle: string;
+  index: number;
+}) => (
   <motion.div
     variants={fadeIn("up", "spring", index * 0.5, 1)}
     className="flex md:flex-row flex-col gap-4"
